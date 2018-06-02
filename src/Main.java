54caddc3f2b0bfe65ac326f03c657870189baa22
ShurinO <arestarx@ukr.net>
2018-06-02 18:18:04 +0300

    lab-2_7_10

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ import com.brainacad.oop.testshapes.Triangle;
 
 public class Main {
     public static void main(String[] args) {
-        Shape shape = new Shape("GREEN");
+        /*Shape shape = new Shape("GREEN");
         System.out.println(shape);
         System.out.println("Shape area is: "+shape.calcArea());
 
@@ -19,7 +19,40 @@ public class Main {
 
         Triangle triangle = new Triangle("RED",5,5,5);
         System.out.println(triangle);
-        System.out.printf("Shape area is: %.3f",triangle.calcArea());
+        System.out.printf("Shape area is: %.3f",triangle.calcArea());*/
+
+        Shape[] arr = {new Rectangle("Black",10,5)
+                ,new Rectangle("Black",9,6)
+                ,new Rectangle("Black",8,7)
+                ,new Rectangle("Black",4,5)
+                ,new Rectangle("Black",5,12)
+                ,new Circle("Green",8.9)
+                ,new Circle("Green",19.2)
+                ,new Triangle("Red",3,4,5)
+                ,new Triangle("Red",12,13,8)};
+        double sumArea = 0;
+        for (Shape shape : arr) {
+            double area = shape.calcArea();
+            sumArea+=area;
+            System.out.printf(shape+", area is: %.5f\n",area);
+        }
+        System.out.printf("All shapes total area: %.5f\n",sumArea);
+        double sumRectArea = 0;
+        double sumTriangleArea = 0;
+        double sumCircleArea = 0;
+        for (Shape shape : arr) {
+            double area = shape.calcArea();
+            if (shape instanceof Rectangle){
+                sumRectArea+=area;
+            }else if (shape instanceof Circle){
+                sumCircleArea+=area;
+            }else if (shape instanceof Triangle){
+                sumTriangleArea+=area;
+            }
+        }
+        System.out.printf("Rectangles total area: %.5f\n",sumRectArea);
+        System.out.printf("Circle total area: %.5f\n",sumCircleArea);
+        System.out.printf("Triangle total area: %.5f\n",sumTriangleArea);
 
     }
 }
