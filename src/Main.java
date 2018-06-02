13364bf0a3f99d38d86e4b2336c9911a19187719
ShurinO <arestarx@ukr.net>
2018-06-02 18:01:08 +0300

    lab-2_7_9

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import com.brainacad.oop.testshapes.Circle;
 import com.brainacad.oop.testshapes.Rectangle;
 import com.brainacad.oop.testshapes.Shape;
+import com.brainacad.oop.testshapes.Triangle;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,11 +11,15 @@ public class Main {
 
         Circle circle = new Circle("YELLOW",10);
         System.out.println(circle);
-        System.out.println("Shape area is: "+circle.calcArea());
+        System.out.printf("Shape area is: %.5f\n",circle.calcArea());
 
         Rectangle rectangle = new Rectangle("BLACK",11,22);
         System.out.println(rectangle);
         System.out.println("Shape area is: "+rectangle.calcArea());
+
+        Triangle triangle = new Triangle("RED",5,5,5);
+        System.out.println(triangle);
+        System.out.printf("Shape area is: %.3f",triangle.calcArea());
 
     }
 }
